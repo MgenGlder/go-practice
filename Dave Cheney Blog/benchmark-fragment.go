c68e204534cacb326f1e406f85db602b8615fbe8
Kunle Oshiyoye <mgenglder@github.com>
2021-09-01 21:14:55 -0400

    Add more go modules (from Dave's blog)

--- Dave Cheney Blog/benchmark-fragment.go
+++ Dave Cheney Blog/benchmark-fragment.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 )
 
+func main() {
+	fmt.Println("Running benchmarkSortStrings..")
+	BenchmarkSortStrings()
+}
+
 func BenchmarkSortStrings(b *testing.B) {
 	s := []string{"heart", "lungs", "brain", "kidneys", "pancreas"} // := is short variable declaration in go
 	b.ReportAllocs()
+	fmt.Println(s)
 	for i := 0; i < b.N; i++ {
 		sort.Strings(s)
 	}
