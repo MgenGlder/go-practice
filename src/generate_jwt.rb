0e52841280691030ca6668f740bcd18d4536bb1f
MgenGlder <mgen.glder@gmail.com>
2023-09-05 00:10:32 -0400

    Add things

--- src/generate_jwt.rb
+++ src/generate_jwt.rb
@@ -2,7 +2,8 @@ require 'openssl'
 require 'jwt'  # https://rubygems.org/gems/jwt
 
 # Private key contents
-private_pem = File.read("./kunle-test-app.2023-09-04.private-key.pem")
+private_pem = ENV["PRIVATE_KEY"]
+puts private_pem
 private_key = OpenSSL::PKey::RSA.new(private_pem)
 
 # Generate the JWT
